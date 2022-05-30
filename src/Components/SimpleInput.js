87ae09b146cb31b6f21097badfd56b721dd565c2
Mwau stephen <stephenmwau41@gmail.com>
2022-05-30 20:21:14 +0800

    added a on change handler on the form input

--- src/Components/SimpleInput.js
+++ src/Components/SimpleInput.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
   return (
     <form>
       <div className="form-control">
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" onChange={nameInputHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
