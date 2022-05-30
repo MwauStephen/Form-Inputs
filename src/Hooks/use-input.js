7e47f0f1e65ed804660d30969840545ef34e5976
Mwau stephen <stephenmwau41@gmail.com>
2022-05-30 23:48:57 +0800

    added generic states on the input hook

--- src/Hooks/use-input.js
+++ src/Hooks/use-input.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
-const UseInput = () => {};
+const UseInput = () => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  retrun;
+};
 
 export default UseInput;
